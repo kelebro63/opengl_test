354afcbb57e6ef12d38680c17850b9ffa898a4d5
Kelebro63 <kelebro63@gmail.com>
2016-05-25 15:08:31 +0300

    add animation

--- OpenGL/app/src/main/java/com/kelebro63/myapplication/OpenGLRenderer.java
+++ OpenGL/app/src/main/java/com/kelebro63/myapplication/OpenGLRenderer.java
@@ -42,18 +42,19 @@ import static android.opengl.GLES20.glViewport;
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     private final static int POSITION_COUNT = 3;
-    private final static long TIME = 10000;
+    private final static long TIME = 10000L;
 
     private Context context;
 
     private FloatBuffer vertexData;
+
     private int uColorLocation;
-    private int aPositionLocation;
     private int uMatrixLocation;
     private int programId;
 
     private float[] mProjectionMatrix = new float[16];
     private float[] mViewMatrix = new float[16];
+    private float[] mModelMatrix = new float[16];
     private float[] mMatrix = new float[16];
 
     public OpenGLRenderer(Context context) {
@@ -82,43 +83,24 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     private void prepareData() {
 
-        float s = 0.4f;
-        float d = 0.9f;
-        float l = 3;
-
         float[] vertices = {
 
-                // первый треугольник
-                -2*s, -s, d,
-                2*s, -s, d,
-                0, s, d,
-
-                // второй треугольник
-                -2*s, -s, -d,
-                2*s, -s, -d,
-                0, s, -d,
-
-                // третий треугольник
-                d, -s, -2*s,
-                d, -s, 2*s,
-                d, s, 0,
-
-                // четвертый треугольник
-                -d, -s, -2*s,
-                -d, -s, 2*s,
-                -d, s, 0,
+                // треугольник
+                -1, -0.5f, 0.5f,
+                1, -0.5f, 0.5f,
+                0, 0.5f, 0.5f,
 
                 // ось X
-                -l, 0,0,
-                l,0,0,
+                -3f, 0, 0,
+                3f, 0, 0,
 
                 // ось Y
-                0,-l,0,
-                0,l,0,
+                0, -3f, 0,
+                0, 3f, 0,
 
                 // ось Z
-                0,0,-l,
-                0,0,l,
+                0, 0, -3f,
+                0, 0, 3f
         };
 
         vertexData = ByteBuffer
@@ -126,11 +108,12 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
                 .order(ByteOrder.nativeOrder())
                 .asFloatBuffer();
         vertexData.put(vertices);
+
     }
 
     private void bindData() {
         // примитивы
-        aPositionLocation = glGetAttribLocation(programId, "a_Position");
+        int aPositionLocation = glGetAttribLocation(programId, "a_Position");
         vertexData.position(0);
         glVertexAttribPointer(aPositionLocation, POSITION_COUNT, GL_FLOAT,
                 false, 0, vertexData);
@@ -150,7 +133,7 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         float bottom = -1;
         float top = 1;
         float near = 2;
-        float far = 8;
+        float far = 12;
         if (width > height) {
             ratio = (float) width / height;
             left *= ratio;
@@ -165,13 +148,10 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void createViewMatrix() {
-        float time = (float)(SystemClock.uptimeMillis() % TIME) / TIME;
-        float angle = time  *  2 * 3.1415926f;
-
         // точка положения камеры
-        float eyeX = (float) (Math.cos(angle) * 4f);
-        float eyeY = 1f;
-        float eyeZ = 4f;
+        float eyeX = 2;
+        float eyeY = 2;
+        float eyeZ = 3;
 
         // точка направления камеры
         float centerX = 0;
@@ -188,40 +168,51 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
 
     private void bindMatrix() {
-        Matrix.multiplyMM(mMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        Matrix.multiplyMM(mMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
+        Matrix.multiplyMM(mMatrix, 0, mProjectionMatrix, 0, mMatrix, 0);
         glUniformMatrix4fv(uMatrixLocation, 1, false, mMatrix, 0);
     }
 
     @Override
     public void onDrawFrame(GL10 arg0) {
-        createViewMatrix();
-        bindMatrix();
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // треугольники
-        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // оси
+        drawAxes();
+
+        // треугольник
+        drawTriangle();
+    }
+
+    private void drawAxes() {
+        Matrix.setIdentityM(mModelMatrix, 0);
+        bindMatrix();
+
+        glLineWidth(3);
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_LINES, 3, 2);
 
         glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 3, 3);
-
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 6, 3);
+        glDrawArrays(GL_LINES, 5, 2);
 
         glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 9, 3);
+        glDrawArrays(GL_LINES, 7, 2);
+    }
 
-        // оси
-        glLineWidth(1);
+    private void drawTriangle() {
+        Matrix.setIdentityM(mModelMatrix, 0);
+        setModelMatrix();
+        bindMatrix();
 
-        glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_LINES, 12, 2);
+        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
 
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_LINES, 14, 2);
 
-        glUniform4f(uColorLocation, 1.0f, 0.5f, 0.0f, 1.0f);
-        glDrawArrays(GL_LINES, 16, 2);
+    private void setModelMatrix() {
+        float angle = (float)(SystemClock.uptimeMillis() % TIME) / TIME * 360;
+        Matrix.rotateM(mModelMatrix, 0, angle, 0, 1, 1);
+        //Matrix.translateM(mModelMatrix, 0, -1, 0, 0); //перемещение
+        //Matrix.scaleM(mModelMatrix, 0, 2, 2, 2);
     }
 }
